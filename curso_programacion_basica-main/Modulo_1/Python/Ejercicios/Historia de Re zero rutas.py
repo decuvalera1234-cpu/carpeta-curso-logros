8b0803ddad69bddf301ac972ebc922db5420aa24
decuvalera1234-cpu <decuvalera1234@gmail.com>
2026-09-17 23:12:40 +0200

    zona del vendedor

---
+++ curso_programacion_basica-main/Modulo_1/Python/Ejercicios/Historia de Re zero rutas.py
@@ -0,0 +1,94 @@
+#inicio de la historia de subaru natsuki
+print("\n" + "="*50)
+print("Rutas de Re Zero")
+print("="*50 + "\n")
+
+print("🧑 Eres Subaru Natsuki")
+print("eres un tipo sin habilidades, ni nada de fuerza, ni poder")
+print("siendo teletrasportado a otro mundo de repente\n")
+
+print("¿Que haces primero?\n")
+
+print("Opcion 1: ver los alrededores")
+print("Opcion 2: hablar con un vendedor")
+print("Opcion 3: quedarte sin hacer nada")
+
+respuesta1 = int(input("entiende que los caminos pueden llevar a tu muerte"))
+#inicia las preguntas de la rutas
+
+if respuesta1 == 1:#listo alrederores
+    print("ten encuentras en un callejon sin salida y encuentras a unos ladrones")
+    print("¿Que haces?\n")
+    print("Opcion 1: luchar")
+    print("Opcion 2: gritar por ayuda")
+    print("Opcion 3: hablar\n")
+    respuesta2 == int(input("Elige tu camino"))
+    if respuesta2 == 1:#listo
+            print("peleas contra los ladrones contra cual arremetes")
+            print("¿Que haces?\n")
+            print("Opcion 1: flaco de 1.67")
+            print("Opcion 2: gordo alto de 1.78")
+            print("Opcion 3: enano de 1.40\n")
+            respuesta3 == int(input("Elige tu camino"))
+            if respuesta3 == 1:#listo flaco
+                print("lo golpeas desprevenido ahora contra quien arremetes si estan aturdidos por el impacto\n")
+                print("Opcion 1: gordo alto de 1.78")
+                print("Opcion 2: enano de 1.40\n")
+                respuesta4 == int(input("Elige tu camino"))
+                if respuesta4 == 1:#listo
+                    print("el enano sale corriendo y les robas el dinero")
+                    print("ahora puedes vivir en una pequeña casa y te montas una pequeña tienda como campesino")
+                    print("fin corto feliz, el camino facil de Subaru Natsuki")
+                elif respuesta4 == 2:#listo
+                    print("gordo alto te da un golpe y te estampa contra la pared")
+                    print("se levanta el flaco y te apuñala")
+                    print("Mueres y Retornas de la Muerte Subaru Natsuki")
+                else:#listo mensaje no valido para las rutas
+                        print("Opcion no validad, tu ruta a terminado con la destrución de tu mente")
+            
+            elif respuesta3 == 2:#listo gordo alto
+                print("lo golpeas desprevenido ahora contra quien arremetes si estan aturdidos por el impacto\n")
+                print("Opcion 1: flaco de 1.67")
+                print("Opcion 2: enano de 1.40\n")
+                respuesta4 == int(input("Elige tu camino"))
+                if respuesta4 == 1:#listo
+                    print("el enano sale corriendo y les robas el dinero")
+                    print("ahora puedes vivir en una pequeña casa y te montas una pequeña tienda como campesino")
+                    print("fin corto feliz, el camino facil de Subaru Natsuki")
+                elif respuesta4 == 2:#listo
+                    print("El flaco se molesta y saca dos dagas")
+                    print("intentas esquivar")
+                    print("y te apuñalan por la costilla")
+                    print("caes al suelo y mueres Subaru Natsuki")
+                else:#listo mensaje no valido para las rutas
+                    print("Opcion no validad, tu ruta a terminado con la destrución de tu mente")
+            if respuesta3 == 3:#listo
+                print("El flaco se molesta y saca dos dagas")
+                print("El gordo arremete")
+                print("Pateas al gordo y no encuentras al flaco")
+                print("El flaco te a apuñalado por la espalda")
+                print("caes al suelo y mueres Subaru Natsuki")      
+            else:#listo mensaje no valido para las rutas
+                print("Opcion no validad, tu ruta a terminado con la destrución de tu mente")  
+    #opciones de la segunda ruta vendedor etc
+    elif respuesta1 == 2:#listo vendedor
+        print("te pregunta si tienes dinero")
+            print("¿Que haces?\n")
+            print("Opcion 1: No soy de por aqui")
+            print("Opcion 2: Necesito ayuda")
+            print("Opcion 3: No tengo dinero\n")
+        if respuesta2 == 1:
+         respuesta1 == 3:#listo quedarse
+        print("Tu mente se corrompe y mueres por la desesperación")
+    
+    else:#listo mensaje no valido para las rutas
+        print("Opcion no validad, tu ruta a terminado con la destrución de tu mente")
+
+    
+
+
+
+
+
+
+
