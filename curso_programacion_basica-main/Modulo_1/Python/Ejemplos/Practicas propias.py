85b71a4771dc3a8a80e1ad52f7a299a8e1d3117b
decuvalera1234-cpu <decuvalera1234@gmail.com>
2026-09-17 12:04:26 -0400

    ddd

--- curso_programacion_basica-main/Modulo_1/Python/Ejemplos/Practicas propias.py
+++ curso_programacion_basica-main/Modulo_1/Python/Ejemplos/Practicas propias.py
@@ -20,9 +20,20 @@ print(saludo.upper())
 print(saludo.capitalize())
 print(saludo.replace(f"mundo",nombre))
 print(saludo.lower())
+print("--------------------------------------------")
 
 #practica 3
+print("hola que tal no tengo tu informacion ¿me la podrias decir?")
+nombre = str(input("dime tu nombre obligado"))
+apellido = str(input("¿dime tu apellido?"))
+edad = int(input("¿dime que edad tienes?"))
+ciudad = str(input("¿donde queda tu rancho?"))
 
+print("--------------------------------------------")
+print(f"entiendo te llamas {nombre} y tu apellido es {apellido}")
+print(f"vaya tienes {edad}")
+print("--------------------------------------------")
+print(f"vives en {ciudad}")
 
 
 
